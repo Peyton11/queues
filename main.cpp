1f59ce0a7b71a5287e1bebc359e5dc89a468dd7b
Peyton11 <peyton.gardner03@gmail.com>
2024-09-27 19:22:27 -0400

    Start user input

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 // main.cpp
-//
+// Program to showcase queues
 
 #include <iostream>
+#include "Bigram.hpp"
+#include "Queue.hpp"
 
 int main() {
+
+    Queue queue;
+
+    int operationChoice;
+    std::cout << "Enqueue[1]\nDequeue[2]\nStatus[3]\nEnd Program[4]\n";
+    std::cout << "Operation choice: ";
+    std::cin >> operationChoice;
+
+    char inputBigram[3];
+    switch (operationChoice) {
+        // Enqueue
+        case 1:
+            std::cout << "Enter a bigram: ";
+            std::cin >> inputBigram;
+            Bigram bigram(inputBigram[0], inputBigram[1]);
+            queue.enqueue(bigram);
+
+        // Dequeue
+        case 2:
+
+        // Status
+        case 3:
+
+        // End Program
+        case 4:
+            return 0;
+    }
 
     return 0;
 }
